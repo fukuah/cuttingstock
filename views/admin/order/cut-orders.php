c0109f49e257f16b29c90515af7a37d5786ed8c8
fukuah <zetx43@gmail.com>
2019-05-18 19:28:50 +0300

    Canvas for cutting visualisation

--- views/admin/order/cut-orders.php
+++ views/admin/order/cut-orders.php
@@ -10,10 +10,61 @@ use yii\helpers\Html;
 
 $this->title = 'Распил на заказы';
 $this->params['breadcrumbs'][] = $this->title;
+
+$canvasAdaptiveCSS = <<<CSS
+#responsive-canvas{
+    width: 100%;
+}
+CSS;
+
+
+$canvasAdaptiveJS = <<<JS
+    function resize(){   
+    var canvas = $("#cutting");
+    canvas.outerHeight($(window).height() - canvas.offset().top - Math.abs(canvas.outerHeight(true) - canvas.outerHeight()));
+  }
+  $(document).ready(function(){
+        var canvas = document.getElementById('responsive-canvas');
+        // var heightRatio = 1.5;
+        // canvas.height = canvas.width * heightRatio;
+        
+	    ctx = canvas.getContext('2d');
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        resize();
+        $(window).on("resize", function(){                      
+            resize();
+        });
+  });
+JS;
+
+
+$this->registerCSS($canvasAdaptiveCSS);
+
+$this->registerJS($canvasAdaptiveJS, \yii\web\View::POS_END)
 ?>
 <div class="order-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    <div>
+        <p></p>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <button id="prev-list">&laquoПредыдущий</button>
+        </div>
+        <div class="col-md-4 text-center">
+            <label aria-label="Лист:">Лист: </label>
+            <input aria-label="Лист" type="text" size="4"/>
+            <button id="go-to-list">Перейти</button>
+        </div>
+        <div class="col-md-4 text-right">
+            <button id="next-list">Следующий&raquo</button>
+        </div>
+        <br>
+        <div id="'outer" class="col-md-12">
+            <canvas id='responsive-canvas'></canvas>
+        </div>
+    </div>
 
     <pre>
         <?php print_r($sheets); ?>
